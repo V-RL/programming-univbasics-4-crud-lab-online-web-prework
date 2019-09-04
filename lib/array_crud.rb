90c2a81f5e05824a5a10a445ff42dfc8a5d2b6a0
Brandon Furr <brandon.furr123@gmail.com>
2019-09-04 05:19:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
     new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
-    
+    maserati_car = new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
