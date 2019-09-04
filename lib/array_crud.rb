b404bdc49d3a7e7be964aada540acf210f824a49
Brandon Furr <brandon.furr123@gmail.com>
2019-09-04 05:27:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,41 +7,41 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+   new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
    new_array << "Lamborghini"
 end
 
 def add_element_to_start_of_array(array, element)
-  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
   new_array.unshift("Lamborghini")
 end
 
 def remove_element_from_end_of_array(array)
-  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
   viper_car = new_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-    new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+    new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
     maserati_car = new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-   new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+   new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
    new_array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
    new_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
    new_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
   new_array[0] = "Camry"
 end
