021fbcb5cfb8b226103563602288670c614b5fe2
Brandon Furr <brandon.furr123@gmail.com>
2019-09-04 05:40:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-    new_array = ["Maserati", "Porsche", "Bugatti", "Viper"]
+    new_array = ["wow", "Maserati", "Porsche", "Bugatti", "Viper"]
     maserati_car = new_array.shift
 end
 
