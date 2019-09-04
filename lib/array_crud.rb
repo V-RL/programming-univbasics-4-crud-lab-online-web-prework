fcce6293212ad9fe7b31e3293fdfc928fa8dc38f
Brandon Furr <brandon.furr123@gmail.com>
2019-09-04 05:16:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_array ["Maserati", "Porsche", "Bugatti", "Viper"]
+  new_array.unshift("Lamborghini")
 end
 
 def remove_element_from_end_of_array(array)
